d66e502e4a6ae4b863fca4283e55981564fd2d79
reneget <ser20051227@gmail.com>
2023-11-11 20:16:30 +0500

    микро баг фиксы добавил вилки и ножи

--- ATATTA_bot.py
+++ ATATTA_bot.py
@@ -30,16 +30,20 @@ def callback_inline(call):
         mes = bot.send_message(call.message.chat.id, spoons_ms[0])
         bot.register_next_step_handler(mes, ankets_creator, spoons=True)
     if call.data == 'Вилки':
-        pass
+        mes = bot.send_message(call.message.chat.id, forks_ms)
+        bot.register_next_step_handler(mes, ankets_creator, forks=True)
+    if call.data == 'Ножи':
+        mes = bot.send_message(call.message.chat.id, knives_ms[1])
+        bot.register_next_step_handler(mes, ankets_creator, knives=True)
 
 
-def ankets_creator(message, cook=None, ladle=None, spoons=None, knives=None, forks=None, dishes=None, cups=None, other_services=None, household_devices=None, parcel_from_Moscow=None, posting_on_a_channel=None, replica_of_dishes=None, technical_support=None, ladle_material=None, spoons_city=None, spoons_work=None):
+def ankets_creator(message, cook=None, ladle=None, spoons=None, knives=None, forks=None, dishes=None, cups=None, other_services=None, household_devices=None, parcel_from_Moscow=None, posting_on_a_channel=None, replica_of_dishes=None, technical_support=None, ladle_material=None, spoons_city=None, spoons_work=None, knives_city=None):
     if cook:
         bot.send_message(message.chat.id, wait_ms)
-        bot.send_message(config.get('admin_group_chat_id'), anket('Повар', message.text, message.from_user.first_name, message.from_user.id), reply_markup=admin_keyboard)
+        bot.send_message(config.get('admin_group_chat_id'), anket('Повар', message.from_user.first_name, message.from_user.id, city=message.text), reply_markup=admin_keyboard)
     if ladle:
         bot.send_message(message.chat.id, wait_ms)
-        bot.send_message(config.get('admin_group_chat_id'), anket('Половник', message.text, message.from_user.first_name, message.from_user.id, ladle_material=ladle_material), reply_markup=admin_keyboard)
+        bot.send_message(config.get('admin_group_chat_id'), anket('Половник', message.from_user.first_name, message.from_user.id, ladle_material=ladle_material, city=message.text), reply_markup=admin_keyboard)
     if spoons:
         mes2 = bot.send_message(message.chat.id, spoons_ms[1])
         bot.register_next_step_handler(mes2, ankets_creator, spoons_city=message.text)
@@ -48,32 +52,56 @@ def ankets_creator(message, cook=None, ladle=None, spoons=None, knives=None, for
         bot.register_next_step_handler(mes3, ankets_creator, spoons_work=message.text, spoons_cit=spoons_city)
     if spoons_work and spoons_city:
         bot.send_message(message.chat.id, wait_ms)
-        bot.send_message(config.get('admin_group_chat_id'), anket('Ложки', spoons_city, message.from_user.first_name, message.from_user.id, spoons_data=[spoons_work, message.text]))
+        bot.send_message(config.get('admin_group_chat_id'), anket('Ложки', message.from_user.first_name, message.from_user.id, spoons_data=[spoons_work, message.text], city=spoons_city), reply_markup=admin_keyboard)
+    if forks:
+        bot.send_message(message.chat.id, wait_ms)
+        bot.send_message(config.get('admin_group_chat_id'), anket('Вилки', message.from_user.first_name, message.from_user.id), reply_markup=admin_keyboard)
+    if knives:
+        mes2 = bot.send_message(message.chat.id, spoons[0])
+        bot.register_next_step_handler(mes2, ankets_creator, knives_city=message.text)
+    if knives_city:
+        bot.send_message(message.chat.id, wait_ms)
+        bot.send_message(config.get('admin_group_chat_id'), anket('Ножи', message.from_user.first_name, message.from_user.id, city=knives_city, knives_set=message.text), reply_markup=admin_keyboard)
 
 
-def anket(objec, city, username, user_id, ladle_material=None, spoons_data=None):
+def anket(objec, username, user_id, ladle_material=None, spoons_data=None, city=None, forks=None, knives_set=None):
     anketa = f'''
-    Объект: {objec}
-    Город: {city}
-    Юзернейм: {username}
-    Юзер ID: {user_id}
-    '''
+        Объект: {objec}
+        Город: {city}
+        Юзернейм: {username}
+        Юзер ID: {user_id}
+        '''
 
     if ladle_material:
         anketa = f'''Объект: {objec}
-        Материал {ladle_material}
-        Город: {city}
-        Юзернейм: {username}
-        Юзер ID: {user_id}
-        '''
+            Материал {ladle_material}
+            Город: {city}
+            Юзернейм: {username}
+            Юзер ID: {user_id}
+            '''
     if spoons_data:
         anketa = f'''Объект: {objec}
-        Город: {city}
-        Что должны сделать {spoons_data[0]}
-        Сумма за доставку {spoons_data[1]}
-        Юзернейм: {username}
-        Юзер ID: {user_id}
-        '''
+            Город: {city}
+            Что должны сделать {spoons_data[0]}
+            Сумма за доставку {spoons_data[1]}
+            Юзернейм: {username}
+            Юзер ID: {user_id}
+            '''
+    if forks:
+        anketa = f'''
+            Объект: {objec}
+            От вилок нужно: {forks}
+            Юзернейм: {username}
+            Юзер ID: {user_id}
+            '''
+    if knives_set:
+        anketa = f'''
+            Объект: {objec}
+            Город: {city}
+            Интересующие ножи: {knives_set}
+            Юзернейм: {username}
+            Юзер ID: {user_id}
+            '''
 
     return anketa
 
